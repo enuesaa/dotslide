bfdb904a8f89136b7d2d4878f836ea237d859fc3
nua <enuesaa@gmail.com>
2024-12-21 17:33:27 +0900

    feat: use pflag as cli parse lib

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 
+	flag "github.com/spf13/pflag"
 	"github.com/enuesaa/dotslide/web"
 )
 
+var (
+	port = flag.IntP("port", "p", 3000, "Port")
+	workdir = flag.StringP("workdir", "w", ".", "Workdir")
+)
 
 func main() {
+	flag.Parse()
+
 	app := web.NewReouter()
 
 	if err := app.Start(":3000"); err != nil {
