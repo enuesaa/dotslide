82dfaa64f43e376e8be38eadee5fcb5c03c94130
nua <enuesaa@gmail.com>
2024-12-22 20:03:03 +0900

    chore: write README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dotslide
+My personal presentation tool. very personal
+
+## Usage
+### 1. Create `.slide.yml`
+Please look up `./testdata` and create `.slide.yml`
+
+### 2. Run `dotslide`
+`dotslide` serve web console for the presentation.
+
+```bash
+$ dotslide -h
+Usage of dotslide:
+      --capture          Make Capture
+  -h, --help             Print help message
+  -p, --port int         Port (default 3000)
+  -w, --workdir string   Workdir (default ".")
+```
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/enuesaa/dotslide/internal"
 	flag "github.com/spf13/pflag"
@@ -11,10 +13,17 @@ var (
 	port    = flag.IntP("port", "p", 3000, "Port")
 	workdir = flag.StringP("workdir", "w", ".", "Workdir")
 	capture = flag.Bool("capture", false, "Make Capture")
+	help    = flag.BoolP("help", "h", false, "Print help message")
 )
 
 func main() {
 	flag.Parse()
+
+	if *help {
+		fmt.Println("Usage of dotslide:")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
 
 	config := internal.Config{
 		Port:    *port,
