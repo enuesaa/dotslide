7d79d750d931c55c5a6d8a1969815693e2be057a
nua <enuesaa@gmail.com>
2024-12-22 15:16:06 +0900

    feat(ui): capture keyboard event

--- src/routes/SlideNextButton.svelte
+++ src/routes/SlideNextButton.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+	import { goto } from '$app/navigation'
 	import { ArrowLeft, ArrowRight } from 'lucide-svelte'
 
 	export let current = 0
 	export let end = 0
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.key === 'ArrowLeft') {
+			if (current > 0) {
+				goto(`?slide=${current - 1}`)
+			}
+        } else if (e.key === 'ArrowRight') {
+			if (current < end) {
+				goto(`?slide=${current + 1}`)
+			}
+        } 
+	}
 </script>
+
+<svelte:window on:keydown={handleKeyDown} />
 
 <div class="flex fixed bottom-3 right-3">
 	{#if current > 0}
