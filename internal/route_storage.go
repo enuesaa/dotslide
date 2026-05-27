8a72b6d2061a56d265396da9482be4799420dd57
nua <enuesaa@gmail.com>
2026-05-27 21:30:39 +0900

    feat: 動画ファイルを再生できるようにする

--- internal/route_storage.go
+++ internal/route_storage.go
@@ -1,8 +1,6 @@
 package internal
 
 import (
-	"mime"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -10,16 +8,9 @@ import (
 )
 
 func (r *Router) handleStorage(c echo.Context) error {
-	path := c.Request().URL.Path // like `/`
+	path := c.Request().URL.Path
 	path = strings.TrimPrefix(path, "/storage/")
 	path = filepath.Join(r.config.Workdir, path)
 
-	fileExt := filepath.Ext(path)
-	f, err := os.ReadFile(path)
-	if err != nil {
-		return err
-	}
-	mimeType := mime.TypeByExtension(fileExt)
-
-	return c.Blob(200, mimeType, f)
+	return c.File(path)
 }
--- src/routes/UnitImage.svelte
+++ src/routes/UnitImage.svelte
@@ -4,10 +4,16 @@
 	export let image: string
 </script>
 
-<img src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${image}`} alt={image} />
+{#if image.endsWith('.mp4')}
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<video src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${image}`} controls playsinline></video>
+{:else}
+	<img src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${image}`} alt={image} />
+{/if}
 
 <style lang="postcss">
-	img {
+	img,
+	video {
 		@apply block mx-auto max-w-[min(100%,1000px)] mb-3;
 	}
 </style>
