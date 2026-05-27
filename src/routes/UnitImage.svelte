6601cb508629c9f9260cb3b0ee72caa3fd764c29
nua <enuesaa@gmail.com>
2026-05-27 21:46:56 +0900

    feat: add size=large option

--- src/routes/UnitImage.svelte
+++ src/routes/UnitImage.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
 	import { PUBLIC_API_ENDPOINT_BASE } from '$env/static/public'
+	import { parseAttrs } from '$lib/attrs'
 
 	export let image: string
+	const { content: filename, attrs } = parseAttrs(image)
 </script>
 
-{#if image.endsWith('.mp4')}
-	<!-- svelte-ignore a11y_media_has_caption -->
-	<video src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${image}`} controls playsinline></video>
-{:else}
-	<img src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${image}`} alt={image} />
-{/if}
+<div class={attrs?.size === 'large' ? 'mb-3 relative large' : 'mb-3 relative'}>
+	{#if filename.endsWith('.mp4')}
+		<!-- svelte-ignore a11y_media_has_caption -->
+		<video src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${filename}`} controls playsinline></video>
+	{:else}
+		<img src={`${PUBLIC_API_ENDPOINT_BASE}/storage/${filename}`} alt={filename}/>
+	{/if}
+</div>
 
 <style lang="postcss">
 	img,
 	video {
-		@apply block mx-auto max-w-[min(100%,1000px)] mb-3;
+		@apply block mx-auto mb-3 w-full;
+		max-width: min(100%, 1000px);
+	}
+
+	.large {
+		width: 100vw;
+		margin-left: calc(50% - 50vw);
+	}
+	.large img,
+	.large video {
+		max-width: min(100vw, 1300px);
 	}
 </style>
