d0e2962b37ffa6d4289baa3a0ba19bc51c86d9cf
nua <enuesaa@gmail.com>
2026-08-26 23:16:24 +0900

    feat: symbol を変えられるように

--- src/routes/UnitCap.svelte
+++ src/routes/UnitCap.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import type { Unit } from '$lib/types'
+	import { parseAttrs } from '$lib/attrs'
 
 	export let unit: Unit
+
+	$: ({ content: cap, attrs } = parseAttrs(unit.cap ?? ''))
+	$: symbol = attrs?.symbol ?? '#'
 </script>
 
 <div class="font-semibold mt-5 mb-3 font-zenmaru text-5xl text-left pl-2 block w-full whitespace-pre-wrap">
-	# {unit.cap}
+	{symbol} {cap}
 </div>
